98941cd40808f3566864b841772114bd0d21ceb5
Mateusz Surowiec <mateuszsurowiec43@gmail.com>
2023-09-27 18:21:42 +0200

    Add function getPlayerChoice() to the script.js file

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function getComputerChoice() {
     let computerArray = ["Rock", "Paper", "Scissors"];
     return computerArray[Math.floor(Math.random() * 3)];
   }
+
+  function getPlayerChoice() {
+    let random = Math.floor(Math.random() * 3);
+    switch (random) {
+      case 0:
+        return "Rock";
+        break;
+      case 1:
+        return "Paper";
+        break;
+      case 2:
+        return "Scissors";
+        break;
+    }
+  }
